d0866e32bac560deee7d0aa6a1f2a7244a452c1d
snays <sbelkhale@yahoo.com>
2016-09-30 14:46:11 -0700

    simple background subtraction added

--- videoLag.cpp
+++ videoLag.cpp
@@ -1,14 +1,14 @@
 /** @file videoLag.cpp
  
- @brief Main function to test different input points and triangulation methods in the TriSubdiv class.
+ @brief Main function to show a cool video editing effect
  @author Sneha Belkhale
  @date September 22nd, 2016
  
- When executing this file, it takes in a command line integer argument from 1 - 10, corresponding to the different points and patterns.
- args 1 : 7 triangulate the image by generating 3 new triangles each time a point is added to the set
- args 7 : 10 simply create triangles by connecting the new point to its nearest 2 neighbors
+ When executing this file, it takes in a command line integer argument corresponding to different background detection methods
+ args 1 : Adaptive gaussian background detection
+ args 2 : Simple background subtraction, press 'space' when you are ready to set the background
+
  
- Displays the ~beautiful nature~ of the triangulation in an animation.
 
  */
 
@@ -32,10 +32,11 @@
 int main(int argc, const char * argv[]) {
     
     /* initializations */
-    
+    std::string arg = argv[1];
     cv::namedWindow("webcam");
     cv::Mat frame; //place holder cv::Mat
     cv::Mat foregroundMask; //place holder cv::Mat
+    cv::Mat background; //for static background method
     cv::Mat dstImage; //place holder cv::Mat
     int keyboard; //keyboard key to stop the program
     int count=0; //counter for debugging purposes
@@ -58,6 +59,24 @@ int main(int argc, const char * argv[]) {
     //create a structuring element for morphological operations on the foreground mask ...
     cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1), cv::Point(erosion_size, erosion_size) );
     
+    //if using the simple background subtractor, ask user to set the background frame when ready
+    if (arg =="2"){
+        cv::Mat background_text;
+        //get the background
+        while((char)keyboard != 32){
+            //make sure the webcam frame is opened correctly
+            if(!stream.read(background)){
+                std::cerr << "Unable to open webcam"<< std::endl;
+                exit(EXIT_FAILURE);
+            }
+            background_text = background.clone();
+            cv::putText(background_text, "press spacebar when ready ",cv::Point(200,200), CV_FONT_HERSHEY_SIMPLEX, 1 ,cv::Scalar(255,255,255),1,8,false);
+            cv::imshow("webcam", background_text);
+            keyboard=cv::waitKey(30);
+        }
+    }
+    
+    
     //Enter a continuous loop until the ESC key is pressed
     while((char)keyboard != 27){
     //while(count<30){ //for debugging
@@ -71,7 +90,13 @@ int main(int argc, const char * argv[]) {
         //instantiate a new matrix to hold the processed frame
         cv::Mat frameToProcess = cv::Mat(frame.rows, frame.cols, CV_8UC3, 0.0);
         //get a mask of the frame foreground region of interest
-        pMOG2->apply(frame, foregroundMask);
+        if (arg == "1")
+            pMOG2->apply(frame, foregroundMask);
+        if (arg == "2") {
+            cv::absdiff(background,frame,foregroundMask);
+            cv::cvtColor(foregroundMask, foregroundMask, CV_BGR2GRAY);
+            cv::threshold(foregroundMask, foregroundMask, 20, 255, cv::THRESH_BINARY);
+        }
         //apply morphological operations to denoise the foreground mask
         //cv::morphologyEx(foregroundMask,foregroundMask,cv::MORPH_OPEN,element);
         //cv::erode(foregroundMask,foregroundMask,element);
@@ -104,15 +129,15 @@ int main(int argc, const char * argv[]) {
             cv::bitwise_and(lagMasks[i],ROI,ROI);
             ROI=lagMasks[i]-ROI;
         
-            /*
-            // Modify the pixels of the HSV image
-            for (int i=0; i<lagFrames[i].rows; i++)
+            
+            // Modify the pixels of the RGB
+            for (int ii=0; ii<lagFrames[i].rows; ii++)
             {
-                for (int j=0; j<lagFrames[i].cols; j++)
+                for (int jj=0; jj<lagFrames[i].cols; jj++)
                 {
-                    lagFrames[i].at<cv::Vec3b>(i,j)[0] += 10;
+                    lagFrames[i].at<cv::Vec3b>(ii,jj)[0] += 10;
                 }
-            }*/
+            }
             
             //copy the past frame to the region of interest
             lagFrames[i].copyTo(temp, ROI);
@@ -129,27 +154,29 @@ int main(int argc, const char * argv[]) {
         }
         
             //cv::cvtColor(frameToProcess,frameToProcess,CV_HSV2BGR);
-
+            //fill the remaining background pixels with the current frame pixels
             cv::cvtColor(frameToProcess,dstImage,CV_BGR2GRAY);
-            //add back in the past
             dstImage.setTo(255, dstImage > 0);
             cv::bitwise_not(dstImage,dstImage);
-            
             frame.copyTo(frameToProcess,dstImage);
         
-            if(lagMasks.size()>5){
-            lagMasks.pop_back();
-                lagFrames.pop_back();}
-
-        cv::imshow("webcam", frameToProcess);
-        cv::waitKey(400);
+            //remove the oldest frame/ foreground mask from the deque
+            if ( lagMasks.size()>5 ) {
+                lagMasks.pop_back();
+                lagFrames.pop_back();
+            }
+            //show the processed frame
+            cv::imshow("webcam", frameToProcess);
+            //set a lag time
+            cv::waitKey(400);
+            //update the counter for debugging purposes
             count++;
-        keyboard=cv::waitKey(30);
-            
+            //get user Key pressed
+            keyboard=cv::waitKey(1);
             
 }
+    //release the webcam after exiting the while loop
     stream.release();
-        
     return 0;
 }
 
